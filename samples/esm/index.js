f44813e8df1912c73c0e7e9af067ce801b82a3f0
ice breaker <1324318532@qq.com>
2024-11-25 22:13:48 +0800

    fix: cjs must require the default export

---
+++ samples/esm/index.js
@@ -0,0 +1,8 @@
+
+import AhoCorasick from 'modern-ahocorasick'
+
+const ac = new AhoCorasick(['keyword1', 'keyword2', 'etc']);
+const results = ac.search('should find keyword1 at position 19 and keyword2 at position 47.');
+
+console.log(results)
+// [ [ 19, [ 'keyword1' ] ], [ 47, [ 'keyword2' ] ] ]
--- tsup.config.ts
+++ tsup.config.ts
@@ -6,4 +6,6 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   clean: true,
   dts: true,
+  cjsInterop: true,
+  splitting: true
 })
